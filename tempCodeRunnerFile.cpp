164f892454e39cfff7e6fb6941103b7c93d64c7f
Rahul <rahulbrt456@gmail.com>
2022-03-27 13:02:50 +0530

    commit

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-    for (int i = n - 1; i > 0; i--)
-    {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
+ // return true;
