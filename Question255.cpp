c0f2f3a23af1b4e0a3a857981ddc80469a487d9f
Rahul Shah <rahulbrt456@gmail.com>
2022-06-04 11:24:30 +0545

    commit

--- Question255.cpp
+++ Question255.cpp
@@ -1,4 +1,4 @@
-// Subset sum equal to 'K' (GFG and Coding Ninjas)
+// Partition Equal Subset Sum(Leetcode Q.416)
 #include <bits/stdc++.h>
 using namespace std;
 void printArray(vector<int> arr)
@@ -39,26 +39,38 @@ bool fun2(int index, int target, vector<int> &arr, vector<vector<int>> &dp)
         take = fun2(index - 1, target - arr[index], arr, dp);
     return dp[index][target] = take || notTake;
 }
-bool subsetSum(vector<int> &arr, int k)
+bool subsetSum(vector<int> &arr)
 {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(k + 1, -1));
-    return fun2(n - 1, k, arr, dp);
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    if (sum % 2)
+        return false;
+    int s1 = sum / 2;
+    vector<vector<int>> dp(n, vector<int>(s1 + 1, -1));
+    return fun2(n - 1, s1, arr, dp);
 }
 // Approach-I(Tabulation)
 // Time->O(n*k)
 // Space->O(n*k)
-bool fun3(vector<int> &arr, int k)
+bool fun3(vector<int> &arr)
 {
     int n = arr.size();
-    vector<vector<int>> dp(n, vector<int>(k + 1, 0));
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    if (sum % 2)
+        return false;
+    int s1 = sum / 2;
+    vector<vector<int>> dp(n, vector<int>(s1 + 1, 0));
     for (int i = 0; i < n; i++)
         dp[i][0] = true;
-    if (k >= arr[0])
+    if (s1 >= arr[0])
         dp[0][arr[0]] = true;
     for (int index = 1; index < n; index++)
     {
-        for (int target = 1; target <= k; target++)
+        for (int target = 1; target <= s1; target++)
         {
             bool notTake = dp[index - 1][target];
             bool take = false;
@@ -67,23 +79,29 @@ bool fun3(vector<int> &arr, int k)
             dp[index][target] = take || notTake;
         }
     }
-    return dp[n - 1][k];
+    return dp[n - 1][s1];
 }
 // Approach-I(Space Optimization)
 // Time->O(n*k)
 // Space->O(k)
-bool fun4(vector<int> &arr, int k)
+bool fun4(vector<int> &arr)
 {
     int n = arr.size();
-    vector<int> prev(k + 1, 0);
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+    if (sum % 2)
+        return false;
+    int s1 = sum / 2;
+    vector<int> prev(s1 + 1, 0);
     prev[0] = true;
-    if (k >= arr[0])
+    if (s1 >= arr[0])
         prev[arr[0]] = true;
     for (int index = 1; index < n; index++)
     {
-        vector<int> curr(k + 1, 0);
+        vector<int> curr(s1 + 1, 0);
         curr[0]=0;
-        for (int target = 1; target <= k; target++)
+        for (int target = 1; target <= s1; target++)
         {
             bool notTake = prev[target];
             bool take = false;
@@ -93,7 +111,7 @@ bool fun4(vector<int> &arr, int k)
         }
         prev = curr;
     }
-    return prev[k];
+    return prev[s1];
 }
 int main()
 {
@@ -106,12 +124,9 @@ int main()
         cin >> arr[i];
     cout << "The Array is: " << endl;
     printArray(arr);
-    int k;
-    cout << "Enter the sum: " << endl;
-    cin >> k;
-    if (fun4(arr, k))
-        cout << "Subset with sum K is present!! " << endl;
+    if (fun4(arr))
+        cout << "The Array can be partitioned into two subsets" << endl;
     else
-        cout << "Not present!!" << endl;
+        cout << "Cannot be partitioned" << endl;
     return 0;
 }
