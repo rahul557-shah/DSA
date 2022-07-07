8fac3e778878ecad26c2658697ef939b054e6a0a
Rahul Shah <rahulbrt456@gmail.com>
2022-07-07 22:02:37 +0530

    commit

--- Question278.cpp
+++ Question278.cpp
@@ -1,4 +1,4 @@
-// Best Time to Buy and Sell the Stock - III (Leetcode Q.123)
+// Best Time to Buy and Sell the Stock - IV (Leetcode Q.188)
 #include <bits/stdc++.h>
 using namespace std;
 void printArray(vector<int> arr)
@@ -22,8 +22,8 @@ int fun1(int index, int buy, int count, vector<int> &A, int n)
     return profit;
 }
 // Approach-I(Memoization)
-// Time-> O(n*2*3)
-// Space-> O(n*2*3)+O(n)
+// Time-> O(n*2*k)
+// Space-> O(n*2*k)+O(n)
 int fun2(int index, int buy, int count, vector<int> &A, int n, vector<vector<vector<int>>> dp)
 {
     if (index == n || count == 0)
@@ -37,25 +37,25 @@ int fun2(int index, int buy, int count, vector<int> &A, int n, vector<vector<vec
         profit = max(A[index] + fun2(index + 1, 1, count - 1, A, n, dp), 0 + fun2(index + 1, 0, count, A, n, dp));
     return dp[index][buy][count] = profit;
 }
-int buyAndSell(vector<int> &A)
+int buyAndSell(vector<int> &A, int k)
 {
     int n = A.size();
-    vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
-    return fun2(0, 1, 2, A, n, dp);
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(k + 1, -1)));
+    return fun2(0, 1, k, A, n, dp);
 }
 // Approach-I(Tabulation)
-// Time-> O(n*2*3)
-// Space-> O(n*2*3)
-int fun3(vector<int> &A)
+// Time-> O(n*2*k)
+// Space-> O(n*2*k)
+int fun3(vector<int> &A, int k)
 {
     int n = A.size();
-    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, 0)));
+    vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(k + 1, 0)));
     int profit = 0;
     for (int index = n - 1; index >= 0; index--)
     {
         for (int buy = 0; buy <= 1; buy++)
         {
-            for (int count = 1; count <= 2; count++)
+            for (int count = 1; count <= k; count++)
             {
                 if (buy == 1)
                     profit = max(-A[index] + dp[index + 1][0][count], 0 + dp[index + 1][1][count]);
@@ -65,22 +65,22 @@ int fun3(vector<int> &A)
             }
         }
     }
-    return dp[0][1][2];
+    return dp[0][1][k];
 }
 // Approach-I(Space Optimization)
-// Time-> O(2*3)
-// Space-> O(2*3)
-int fun4(vector<int> &A)
+// Time-> O(n2*k)
+// Space-> O(2*k)
+int fun4(vector<int> &A, int k)
 {
     int n = A.size();
-    vector<vector<int>> next(2, vector<int>(3, 0));
+    vector<vector<int>> next(2, vector<int>(k + 1, 0));
     int profit = 0;
     for (int index = n - 1; index >= 0; index--)
     {
         vector<vector<int>> curr(2, vector<int>(3, 0));
         for (int buy = 0; buy <= 1; buy++)
         {
-            for (int count = 1; count <= 2; count++)
+            for (int count = 1; count <= k; count++)
             {
                 if (buy == 1)
                     profit = max(-A[index] + next[0][count], 0 + next[1][count]);
@@ -91,7 +91,7 @@ int fun4(vector<int> &A)
         }
         next = curr;
     }
-    return next[1][2];
+    return next[1][k];
 }
 int main()
 {
@@ -104,6 +104,9 @@ int main()
         cin >> A[i];
     cout << "The Array is: " << endl;
     printArray(A);
-    cout << "Maximum Profit is: " << fun4(A) << endl;
+    int k;
+    cout << "Enter the value of k: " << endl;
+    cin >> k;
+    cout << "Maximum Profit is: " << buyAndSell(A, k) << endl;
     return 0;
 }
