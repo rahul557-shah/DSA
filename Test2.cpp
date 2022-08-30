dc0552bf27f3fb1408feb0fac9c95e540b7fc3a3
Rahul Shah <rahulbrt456@gmail.com>
2022-08-30 08:55:51 +0530

    commit

--- Test2.cpp
+++ Test2.cpp
@@ -1,65 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-void mergeAll(vector<int> &A, int low, int mid, int high)
+void printArray(vector<int> A, int n)
 {
-    int i = low, j = mid + 1, k = low;
-    vector<int> B(high + 1);
-    while (i <= mid && j <= high)
+    for (int i = 0; i < n; i++)
     {
-        if (A[i] <= A[j])
-            B[k++] = A[i++];
+        cout << A[i] << " ";
+    }
+    cout << endl;
+}
+vector<int> removeDuplicates(vector<int> &A, int n)
+{
+    int i = 0, j = 1;
+    while (j < n)
+    {
+        if (A[i] == A[j])
+            j++;
         else
-            B[k++] = A[j++];
-    }
-    while (i <= mid)
-        B[k++] = A[i++];
-    while (j <= high)
-        B[k++] = A[j++];
-    for (int i = low; i <= high; i++)
-        A[i] = B[i];
-}
-void mergeSort(vector<int> &A, int low, int high)
-{
-    if (low < high)
-    {
-        int mid = (low + high) / 2;
-
-        mergeSort(A, low, mid);
-        mergeSort(A, mid + 1, high);
-
-        mergeAll(A, low, mid, high);
-    }
-}
-int partitionFun(vector<int> &A, int low, int high)
-{
-    int i = low - 1, pivot = A[high];
-    for (int j = low; j <= high - 1; j++)
-    {
-        if (A[j] < pivot)
         {
             i++;
-            swap(A[i], A[j]);
+            A[i] = A[j];
         }
     }
-    swap(A[i + 1], A[high]);
-    return i + 1;
-}
-void quickSort(vector<int> &A, int low, int high)
-{
-    if (low < high)
-    {
-        int partition = partitionFun(A, low, high);
-        quickSort(A, low, partition - 1);
-        quickSort(A, partition + 1, high);
-    }
+    int sz = i + 1;
+    vector<int> temp(sz);
+    for (int i = 0; i < sz; i++)
+        temp[i] = A[i];
+    return temp;
 }
 int main()
 {
-    vector<int> A = {3, 1, 5, 2, 7, 4, 6, 9, 8, 10};
-    int n = A.size();
-    quickSort(A, 0, n - 1);
+    int n;
+    cout << "Enter size: " << endl;
+    cin >> n;
+    vector<int> A(n);
+    cout << "Enter elements: " << endl;
     for (int i = 0; i < n; i++)
-        cout << A[i] << " ";
-    cout << endl;
+    {
+        cin >> A[i];
+    }
+    cout << "Original Array is: " << endl;
+    printArray(A, n);
+    vector<int> ans = removeDuplicates(A, n);
+    cout << "After Removing Duplicates Array is: " << endl;
+    printArray(ans, ans.size());
     return 0;
 }
